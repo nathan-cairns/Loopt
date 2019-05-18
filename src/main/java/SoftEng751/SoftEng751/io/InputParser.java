090c6f52cd3a215f070d69118422001d1819545a
Nathan Cairns <nathancairns777@gmail.com>
2019-05-18 17:17:35 +1200

    Add io interfaces

---
+++ src/main/java/SoftEng751/SoftEng751/io/InputParser.java
@@ -0,0 +1,9 @@
+package SoftEng751.SoftEng751.io;
+
+import SoftEng751.SoftEng751.testMethods.LoopVar;
+
+import java.util.List;
+
+public interface InputParser {
+    public List<LoopVar> parse(String filepath);
+}
---
+++ src/main/java/SoftEng751/SoftEng751/io/OutputParser.java
@@ -0,0 +1,9 @@
+package SoftEng751.SoftEng751.io;
+
+import SoftEng751.SoftEng751.testMethods.LoopVar;
+
+import java.util.List;
+
+public interface OutputParser {
+    public void output(List<LoopVar> transformedLoopVars);
+}
