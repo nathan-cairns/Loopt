0f0bf02445a47501aa99d4861911a473a8be76a9
Eugene Bulog <ebul920@UoA.auckland.ac.nz>
2019-05-18 17:36:23 +1200

    changed output interface

---
+++ src/main/java/SoftEng751/SoftEng751/io/DefaultOutParser.java
@@ -0,0 +1,15 @@
+package SoftEng751.SoftEng751.io;
+
+import java.util.List;
+
+import SoftEng751.SoftEng751.testMethods.LoopVar;
+import spoon.reflect.code.CtFor;
+
+public class DefaultOutParser implements OutputParser {
+
+	public void output(List<LoopVar> transformedLoopVars, CtFor originalLoop) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
--- src/main/java/SoftEng751/SoftEng751/io/OutputParser.java
+++ src/main/java/SoftEng751/SoftEng751/io/OutputParser.java
@@ -1,9 +1,10 @@
 package SoftEng751.SoftEng751.io;
 
 import SoftEng751.SoftEng751.testMethods.LoopVar;
+import spoon.reflect.code.CtFor;
 
 import java.util.List;
 
 public interface OutputParser {
-    void output(List<LoopVar> transformedLoopVars);
+    void output(List<LoopVar> transformedLoopVars, CtFor originalLoop);
 }
