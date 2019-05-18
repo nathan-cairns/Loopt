6337962562ed6c888655cfc532411660335f9a37
Nathan Cairns <nathancairns777@gmail.com>
2019-05-18 17:23:42 +1200

    Change test bounds

--- src/main/java/SoftEng751/SoftEng751/testClass.java
+++ src/main/java/SoftEng751/SoftEng751/testClass.java
@@ -5,18 +5,11 @@ public class testClass {
 	int[][] A;
 	
 	public void transformLoop(){
-
-	    	for(int i = 0; i < 6; i++){
-	    		
-	    		for(int j = 0; j < 5; j++)
-	    		{
-
-	    			A[i][j] = A[i-1][j+1] + 1;
-
-	    		}
-	    		
-	    	}
-		
+		for(int i = 1; i < 6; i++) {
+			for (int j = 1; j < 5; j++) {
+				A[i][j] = A[i - 1][j + 1] + 1;
+			}
+		}
 	} 
 	
 }
