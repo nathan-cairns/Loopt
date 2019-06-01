f27b6240aa51eb16b8c00f33150c9058daef9c9f
Eugene-Bulog <eugene.bulog@gmail.com>
2019-06-01 20:16:15 +1200

    added cleaner comments for outputparser classes

--- src/main/java/SoftEng751/SoftEng751/io/DefaultOutParser.java
+++ src/main/java/SoftEng751/SoftEng751/io/DefaultOutParser.java
@@ -19,33 +19,42 @@ public class DefaultOutParser implements OutputParser {
 		
 		CtFor loop = originalLoop;
 		
+		// For each loop in the code
 		for (LoopVar loopVar : transformedLoopVars) {
 			
+			// Create the lower and upper bounds of the loop conditions
 			String lowerBound = loopVar.boundsName.replaceAll(loopVar.name + "1", Integer.toString(loopVar.lowerbound));
 			String upperBound = loopVar.boundsName.replaceAll(loopVar.name + "1", Integer.toString(loopVar.upperbound));
 			
+			// Create the initializion for the loop conditions
 			CtStatement forInit = spoon.getFactory().Code().createCodeSnippetStatement("int " + loopVar.name + "1 = " + lowerBound);
 			List<CtStatement> forInitList = new ArrayList<CtStatement>();
 			forInitList.add(forInit);
 			
+			// Create the loop expression
 			CtExpression<Boolean> forExp = spoon.getFactory().createCodeSnippetExpression(loopVar.name + "1 < " + upperBound);
 			
+			// Create the loop update
 			CtStatement forUpdate = spoon.getFactory().Code().createCodeSnippetStatement(loopVar.name + "1++");
 			List<CtStatement> forUpdateList = new ArrayList<CtStatement>();
 			forUpdateList.add(forUpdate);
 			
+			// Assemble the loop statement
 			loop.setExpression(forExp);
 			loop.setForInit(forInitList);
 			loop.setForUpdate(forUpdateList);
 			
+			// set the next loop to rewrite as the next nested loop (if it exists)
 			List<CtFor> loopElements = loop.getBody().getElements(new TypeFilter<CtFor>(CtFor.class));
 			if (!loopElements.isEmpty()) {
 				loop = loopElements.get(0);
 			}
 		}
 		
+		// Need to write the "i" and "j" (or equivalent variables) in reverse order to how they're stored
 		Collections.reverse(transformedLoopVars);
 		
+		// Define "i" and "j" (or equivalent vars) as functions of the transformed variables
 		for (LoopVar loopVar : transformedLoopVars) {
 			CtStatement varDefine = spoon.getFactory().Code().createCodeSnippetStatement("int " + loopVar.name + " = " + loopVar.transformedname);
 			loop.getBody().insertBefore(varDefine);
--- src/main/java/SoftEng751/SoftEng751/io/OutputParser.java
+++ src/main/java/SoftEng751/SoftEng751/io/OutputParser.java
@@ -6,5 +6,12 @@ import spoon.reflect.code.CtFor;
 import java.util.List;
 
 public interface OutputParser {
+	
+	/**
+	 * Get the new transformed java code in string form
+	 * @param transformedLoopVars The loop vars with set transformed name
+	 * @param originalLoop The original loop code that was input into the system
+	 * @return The transformed code (in string form)
+	 */
     String output(List<LoopVar> transformedLoopVars, CtFor originalLoop);
 }
